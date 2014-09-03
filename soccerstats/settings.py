ac8c07f6ab01a21aedf740b721a772492c125b6a
Anuvrat Singh <anuvrat@amazon.com>
2014-09-03 19:22:17 +0530

    Created project using scrapy

---
+++ soccerstats/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for soccerstats project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'soccerstats'
+
+SPIDER_MODULES = ['soccerstats.spiders']
+NEWSPIDER_MODULE = 'soccerstats.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'soccerstats (+http://www.yourdomain.com)'
